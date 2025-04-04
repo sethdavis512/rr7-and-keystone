54aad37901bfed13cef30f72da167a57a4cc21ed
Seth Davis <sethdavis512@gmail.com>
2025-04-04 13:04:48 -0500

    Get codegen and type safety working

--- web/app/routes/home.tsx
+++ web/app/routes/home.tsx
@@ -1,13 +1,34 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { client } from '~/utils/graphql.server';
+import {
+    GetKitchenSinkDocument,
+    type GetKitchenSinkQuery
+} from '~/generated/graphql';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: 'New React Router App' },
+        { name: 'description', content: 'Welcome to React Router!' }
+    ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function loader() {
+    const { posts, customers, users } =
+        await client.request<GetKitchenSinkQuery>(GetKitchenSinkDocument);
+
+    return {
+        customers,
+        posts,
+        users
+    };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+    return (
+        <>
+            <pre>
+                <code>{JSON.stringify(loaderData, null, 4)}</code>
+            </pre>
+        </>
+    );
 }
