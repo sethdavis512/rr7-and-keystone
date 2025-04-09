c6abd88c69ab273f49b88df36d0602cffda867c2
Seth Davis <sethdavis512@gmail.com>
2025-04-08 23:25:37 -0500

    Add cors to proxy

--- proxy/src/index.ts
+++ proxy/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { proxy } from 'hono/proxy';
 import { html } from 'hono/html';
+import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server';
 
 const app = new Hono();
@@ -13,8 +14,8 @@ const view = html`
     </html>
 `;
 
+app.use('/api/*', cors());
 app.get('/', (c) => c.html(view));
-
 app.all('/api/:path', (c) => {
     return proxy(
         `https://${process.env.KEYSTONE_INTERNAL_URL!}/api/${c.req.param(
