c0d4bf131b3e84a9b74e50351afef5fef44eb234
Seth Davis <sethdavis512@gmail.com>
2025-04-09 21:07:07 -0500

    Set a content type and method

--- web/app/utils/graphql.server.ts
+++ web/app/utils/graphql.server.ts
@@ -1,3 +1,8 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const client = new GraphQLClient(process.env.VITE_GRAPHQL_ENDPOINT!);
+export const client = new GraphQLClient(process.env.VITE_GRAPHQL_ENDPOINT!, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
