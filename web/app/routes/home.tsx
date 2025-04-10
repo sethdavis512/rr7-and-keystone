6a76375c37e76b24584ceb52e79be9f318be4b2f
Seth Davis <sethdavis512@gmail.com>
2025-04-09 23:03:08 -0500

    Add client side cache

--- web/app/routes/home.tsx
+++ web/app/routes/home.tsx
@@ -4,6 +4,7 @@ import {
     GetKitchenSinkDocument,
     type GetKitchenSinkQuery
 } from '~/generated/graphql';
+import { FlatCache } from 'flat-cache';
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -28,6 +29,54 @@ export async function loader() {
         return { status: 'ERROR' };
     }
 }
+
+const cache = new FlatCache();
+
+let isInitialRequest = true;
+
+export async function clientLoader({
+    request,
+    serverLoader
+}: Route.ClientLoaderArgs) {
+    const cacheKey = request.url;
+
+    if (isInitialRequest) {
+        isInitialRequest = false;
+
+        const serverData = await serverLoader();
+
+        cache.setKey(cacheKey, serverData);
+        cache.save();
+
+        return serverData;
+    }
+
+    const cachedData = cache.getKey(cacheKey);
+
+    if (cachedData) {
+        return cachedData;
+    }
+
+    const serverData = await serverLoader();
+
+    cache.setKey(cacheKey, serverData);
+    cache.save();
+
+    return serverData;
+}
+
+clientLoader.hydrate = true;
+
+// export async function clientAction({
+//     request,
+//     serverAction
+// }: Route.ClientActionArgs) {
+//     const cacheKey = request.url;
+//     cache.removeKey(cacheKey);
+//     cache.save();
+//     const serverData = await serverAction();
+//     return serverData;
+// }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
     return (
